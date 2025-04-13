4fb87cce1297bf8e2461a26888d685a0ead88a0f
Arrhat Maharjan <arhato23@gmail.com>
2025-04-13 01:19:38 +0100

    Animatiion for player character added.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     
     private Rigidbody2D rb;
     private bool isGrounded;
+    float horizontalInput;
+    bool isFacingRight = true;
     
     public Transform leftFoot;
     public Transform rightFoot;
@@ -14,50 +16,79 @@ public class PlayerMovement : MonoBehaviour
     public float groundCheckDistance = 0.1f;
     public LayerMask groundLayer;
     
+    Animator animator;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         rb.freezeRotation = true;
     }
-    
+
     void Update()
     {
-        CheckGrounded();
-        
+        // CheckGrounded();
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        FlipSprite();
+
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             Jump();
+            animator.SetBool("isJumping", !isGrounded);
         }
-        
-        Move();
     }
     
-    void CheckGrounded()
+    void FixedUpdate()
     {
-        if (rb.linearVelocity.y <= 0.1f)
-        {
-            bool leftGrounded = Physics2D.Raycast(leftFoot.position, Vector2.down, groundCheckDistance, groundLayer);
-            bool rightGrounded = Physics2D.Raycast(rightFoot.position, Vector2.down, groundCheckDistance, groundLayer);
-            
-            isGrounded = leftGrounded || rightGrounded;
-            
-            Debug.DrawRay(leftFoot.position, Vector2.down * groundCheckDistance, leftGrounded ? Color.green : Color.red);
-            Debug.DrawRay(rightFoot.position, Vector2.down * groundCheckDistance, rightGrounded ? Color.green : Color.red);
-        }
-        else
-        {
-            isGrounded = false;
-        }
+        Move();
     }
+    // void CheckGrounded()
+    // {
+    //     if (rb.linearVelocity.y <= 0.1f)
+    //     {
+    //         bool leftGrounded = Physics2D.Raycast(leftFoot.position, Vector2.down, groundCheckDistance, groundLayer);
+    //         bool rightGrounded = Physics2D.Raycast(rightFoot.position, Vector2.down, groundCheckDistance, groundLayer);
+    //         
+    //         isGrounded = leftGrounded || rightGrounded;
+    //         
+    //         Debug.DrawRay(leftFoot.position, Vector2.down * groundCheckDistance, leftGrounded ? Color.green : Color.red);
+    //         Debug.DrawRay(rightFoot.position, Vector2.down * groundCheckDistance, rightGrounded ? Color.green : Color.red);
+    //     }
+    //     else
+    //     {
+    //         isGrounded = false;
+    //     }
+    // }
     
     void Move()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.linearVelocity.x));
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
+
+    }
+
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
     }
     
     void Jump()
     {
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        isGrounded = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+        animator.SetBool("isJumping", !isGrounded);
     }
 }
